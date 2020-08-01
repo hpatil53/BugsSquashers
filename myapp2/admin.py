28b9220825fe125767d135c580b3d7a511473931
chndpreeti <46122725+chndpreeti@users.noreply.github.com>
2020-08-02 00:11:17 +0530

    myapp2 Setting files
    setting files for myapp2

---
+++ myapp2/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import HWRegister ,Anganwadi_details
+
+# Register your models here.
+
+admin.site.register(HWRegister)
+admin.site.register(Anganwadi_details)
